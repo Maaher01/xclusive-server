ac471faff213fe06a421847f4f0c48f2fa1ce072
Maaher01 <maaher1145@gmail.com>
2025-09-24 10:56:55 +0600

    Unused import removed

--- app/Listeners/OrderConfirmationMail.php
+++ app/Listeners/OrderConfirmationMail.php
@@ -5,7 +5,6 @@ namespace App\Listeners;
 use App\Events\OrderPlaced;
 use App\Mail\OrderConfirmationEmail;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Mail;
 
 class OrderConfirmationMail implements ShouldQueue
